d6d1b417d69bfe3cbc47171abac81991ac9c5e0e
JDuong70 <90360057+JDuong70@users.noreply.github.com>
2021-09-08 20:13:40 -0400

    Add files via upload

---
+++ Week 2 MGS 314 Recitation.py
@@ -0,0 +1,21 @@
+MGS=15.7
+
+print(MGS-5)
+
+my_string='welcome'
+
+print(my_string)
+
+print(type(my_string))
+
+
+points_obtained=100
+
+total_points=500
+
+percentage=((points_obtained/total_points)*100)
+
+print(percentage)
+
+x="I'm a UB student"
+print(x[-7])
